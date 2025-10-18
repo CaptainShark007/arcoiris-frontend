4c6a2b0018d9ed41c3674544662348d67f9c23bc
Diego <anikilogomez123@gmail.com>
2025-10-18 15:28:31 -0300

    feat(shop/pages): integra la lista de productos y filtros en ShopPage

--- src/features/shop/pages/ShopPage.tsx
+++ src/features/shop/pages/ShopPage.tsx
@@ -1,13 +1,39 @@
+import { useState } from 'react';
 import { ProductListPage } from '../components';
-import { useProducts } from '../hooks';
+//import { useProducts } from '../hooks';
+import { useFilteredProducts } from '../hooks/products/useFilteredProducts';
 
 const ShopPage = () => {
-  const { products = [], isLoading } = useProducts();
+  //const { products = [], isLoading } = useProducts();
+  const [page, setPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(8); // nuevo
+  const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
 
+  const {
+    data: products = [],
+    isLoading,
+    totalProducts,
+  } = useFilteredProducts({
+    page,
+    brands: selectedBrands,
+    itemsPerPage // nuevo
+  });
+
+  {
+    /* <ProductListPage products={products} isLoading={isLoading} /> */
+  }
   return (
-    <>
-      <ProductListPage products={products} isLoading={isLoading} />
-    </>
+    <ProductListPage
+      products={products}
+      isLoading={isLoading}
+      page={page}
+      setPage={setPage}
+      itemsPerPage={itemsPerPage}
+      setItemsPerPage={setItemsPerPage}
+      totalProducts={totalProducts}
+      selectedBrands={selectedBrands}
+      setSelectedBrands={setSelectedBrands}
+    />
   );
 };
 
