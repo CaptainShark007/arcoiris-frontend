34e3362368bc9d284a93d238ca767032b7d4c88d
Diego <anikilogomez123@gmail.com>
2025-10-18 17:46:47 -0300

    feat(shop/hooks): agrega useCategories y actualiza useFilteredProducts

--- src/features/shop/hooks/products/useFilteredProducts.ts
+++ src/features/shop/hooks/products/useFilteredProducts.ts
@@ -5,15 +5,17 @@ import { useQuery } from '@tanstack/react-query';
 export const useFilteredProducts = ({
   page,
   brands,
+  categoriesIds,
   itemsPerPage = 8, // nuevo
 }: {
   page: number;
   brands?: string[];
+  categoriesIds?: string[]; // nuevo
   itemsPerPage?: number; // nuevo
 }) => {
   const { data, isLoading } = useQuery({
-    queryKey: ['filteredProducts', page, brands, itemsPerPage], // itemsPerPage nuevo
-    queryFn: () => getFilteredProducts({ page, brands, itemsPerPage }), // itemsPerPage nuevo
+    queryKey: ['filteredProducts', page, brands, categoriesIds, itemsPerPage], // itemsPerPage nuevo
+    queryFn: () => getFilteredProducts({ page, brands, categoriesIds, itemsPerPage }), // itemsPerPage nuevo
     retry: false, // no reintentar en caso de error
   });
 
