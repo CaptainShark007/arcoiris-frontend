ae7e4e8e6cf032ecb6563ab722475dfc8b641cb8
Diego <anikilogomez123@gmail.com>
2025-10-23 14:34:51 -0300

    feat(rutas): actualiza enrutamiento y carga perezosa

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,7 +1,8 @@
 import { Error404 } from '@shared/components/Error404';
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from '../layout/MainLayout';
-import { ContactPage, HomePage, ProductPage, ShopPage } from './lazy';
+import { ClientLayout } from '../layout/ClientLayout';
+import { ContactPage, HomePage, LoginPage, OrderUserPage, ProductPage, RegisterPage, ShopPage } from './lazy';
 
 export default function Router() {
   return (
@@ -11,8 +12,29 @@ export default function Router() {
         <Route path='tienda' element={<ShopPage />} />
         <Route path='tienda/:slug' element={<ProductPage />} />
         <Route path='contacto' element={<ContactPage />} />
+        <Route path='acceder' element={<LoginPage />} />
+        <Route path='registrarse' element={<RegisterPage />} />
         <Route path='*' element={<Error404 />} />
+      </Route>
+      <Route path='/cuenta' element={<ClientLayout />} >
+        <Route path='pedidos' element={<OrderUserPage />} />
       </Route>
     </Routes>
   );
 }
+
+/*
+debo lograr similar a esto
+patch: 'cuenta',
+element: <ClientLayout />,
+children: [
+  {
+    path: '',
+    element: <Navigate to='/cuenta/pedidos' />
+  },
+  {
+    path: 'pedidos',
+    element: <OrderUserPage />
+  }
+]
+*/
