98da5aa6403886135dfea7830212a2b4d3b2e203
Diego <anikilogomez123@gmail.com>
2025-10-23 16:08:04 -0300

    feat(orders): maquetado inicial de la página de órdenes del usuario

--- src/features/orders/pages/OrderUserPage.tsx
+++ src/features/orders/pages/OrderUserPage.tsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router";
+
 const OrderUserPage = () => {
   return (
-    <div>OrderUserPage</div>
-  )
-}
+    <div>
+      <div>
+        <h1>Pedidos</h1>
+        <span>30</span>
+      </div>
+      {
+        [].length === 0 ? (
+          <>
+            <p>Todavia no has hecho ningun pedido</p>
+            <Link to="/tienda">Empezar a comprar</Link>
+          </>
+        ) : (
+          <div>tabla de ordenes</div>
+        )
+      }
+    </div>
+  );
+};
 
 export default OrderUserPage;
